96339283c31c782e32f747b6fc1b24241f23e8b3
Samuel Scheidegger <scheidegger.samuel@gmail.com>
2018-01-24 17:06:35 +0100

    :bug:

--- init_backend.py
+++ init_backend.py
@@ -13,7 +13,7 @@ def push_credentials(name, network, local_file='client_credentials.txt', remote_
     clients = manager.list_search({"name": name})
 
     ssh = SSHClient()
-    ssh.load_system_host_keys()
+    # ssh.load_system_host_keys()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
     for c in clients:
