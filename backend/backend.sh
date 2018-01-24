a269d733bc5686a7a59f5a633cbb306385f0fe57
Samuel Scheidegger <scheidegger.samuel@gmail.com>
2018-01-24 16:51:45 +0100

    :bug:

--- backend/backend.sh
+++ backend/backend.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# set hostname 
+# set hostname
 sudo echo cloud-backend > /etc/hostname
 sudo sed -i "s/127.0.0.1 localhost/127.0.0.1 cloud-backend/g" /etc/hosts
 
@@ -14,3 +14,11 @@ sudo pip install psutil
 
 # echo "Cloning repo with WASP"
 git clone https://github.com/TommyTram/wasp_cloud.git ~ubuntu/wasp_cloud
+
+
+while [ ! -f ~ubuntu/credentials.txt ] ;
+do
+      sleep 2
+done
+
+python ~ubuntu/wasp_cloud/backend/backend.py
