b6bcb8b4645444086cb46abc8731e6278b2a5e24
Ivo <ivo.batkovic@zenuity.com>
2018-01-24 16:33:18 +0100

    Added flask server on the backend to report status if it is busy or not.

--- backend/backend.py
+++ backend/backend.py
@@ -3,42 +3,68 @@ import pika
 from optparse import OptionParser
 import ConfigParser
 import time
+from flask import Flask
+import threading
+
+app = Flask(__name__)
+isBusyState = "0"
+
+@app.route("/isBusy")
+def isBusy():
+    return isBusyState
 
 def callback(ch, method, properties, body):
-	print(" [x] Processing %r" % body)
+        global isBusyState
+        isBusyState = "1"
+        print(" [x] Processing %r" % body)
 
-	ch.basic_ack(delivery_tag = method.delivery_tag)
-	print(" [x] Process done ---")
-	
+        ch.basic_ack(delivery_tag = method.delivery_tag)
+        time.sleep(3)
+        print(" [x] Process done ---")
+        isBusyState = "0"
+
 def receive(connection_info=None):
-	qname = "wasp"
-	credentials = pika.PlainCredentials(connection_info["username"], connection_info["password"])
-	connection = pika.BlockingConnection(pika.ConnectionParameters(connection_info["server"],connection_info["port"],'/',credentials))
-	channel = connection.channel()
+        qname = "wasp"
+        credentials = pika.PlainCredentials(connection_info["username"], connection_info["password"])
+        connection = pika.BlockingConnection(pika.ConnectionParameters(connection_info["server"],connection_info["port"],'/',credentials))
+        channel = connection.channel()
 
-	channel.queue_declare(queue=qname, durable=True)
-	channel.basic_qos(prefetch_count=1)
-	channel.basic_consume(callback, queue=qname)
-	print(' [*] Waiting for messages. To exit press CTRL+C')
-	channel.start_consuming()
+        channel.queue_declare(queue=qname, durable=True)
+        channel.basic_qos(prefetch_count=1)
+        channel.basic_consume(callback, queue=qname)
+        print(' [*] Waiting for messages. To exit press CTRL+C')
+        channel.start_consuming()
 
 
 if __name__=="__main__":
-	parser = OptionParser()
-	parser.add_option('-c', '--credential', dest='credentialFile', help='Path to CREDENTIAL file', metavar='CREDENTIALFILE')
-	(options, args) = parser.parse_args()
-	
-	if options.credentialFile:
-		config = ConfigParser.RawConfigParser()
-		config.read(options.credentialFile)
-		connection = {}
-		connection["server"] = config.get('rabbit', 'server')
-		connection["port"] = int(config.get('rabbit', 'port'))
-		connection["queue"] = config.get('rabbit', 'queue')
-		connection["username"] = config.get('rabbit', 'username')
-		connection["password"] = config.get('rabbit', 'password')
-		receive(connection_info=connection)
-	else:
-		#e.g. python backend.py -c credentials.txt
-		print("Syntax: 'python backend.py -h' | '--help' for help")
+        parser = OptionParser()
+        parser.add_option('-c', '--credential', dest='credentialFile', help='Path to CREDENTIAL file', metavar='CREDENTIALFILE')
+        (options, args) = parser.parse_args()
 
+        jobs = []
+        if options.credentialFile:
+                config = ConfigParser.RawConfigParser()
+                config.read(options.credentialFile)
+                connection = {}
+                connection["server"] = config.get('rabbit', 'server')
+                connection["port"] = int(config.get('rabbit', 'port'))
+                connection["queue"] = config.get('rabbit', 'queue')
+                connection["username"] = config.get('rabbit', 'username')
+                connection["password"] = config.get('rabbit', 'password')
+
+                thread = threading.Thread(target=receive,args=(connection,))
+                jobs.append(thread)
+                print("Starting flask also")
+                thread = threading.Thread(target=app.run,args=("0.0.0.0",))
+                jobs.append(thread)
+        else:
+                #e.g. python backend.py -c credentials.txt
+                print("Syntax: 'python backend.py -h' | '--help' for help")
+        for j in jobs:
+                j.start()
+         # Ensure all of the threads have finished
+        for j in jobs:
+                j.join()
+
+
+                
