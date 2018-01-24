ada6b938c03ee8116f108e1635d044d12bdd8234
Samuel Scheidegger <scheidegger.samuel@gmail.com>
2018-01-24 12:32:53 +0100

    rabbitmq

--- backend/backend.sh
+++ backend/backend.sh
@@ -1,18 +1,14 @@
 #!/bin/sh
 
 # set hostname 
-sudo echo waspmq-backend > /etc/hostname
-sudo sed -i "s/127.0.0.1 localhost/127.0.0.1 waspmq-backend/g" /etc/hosts
+sudo echo cloud-backend > /etc/hostname
+sudo sed -i "s/127.0.0.1 localhost/127.0.0.1 cloud-backend/g" /etc/hosts
 
 # install some dependencies
 sudo apt-get -y update
 sudo apt-get install -y python-dev
 sudo apt-get install -y python-pip
 sudo apt-get install -y python-pika
-
-# prepare directory 
-mkdir /usr/local/
-cd /usr/local/
 
 # echo "Cloning repo with WASP"
 git clone https://github.com/TommyTram/wasp_cloud.git
---
+++ start_rabbitmq.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+python vmanager.py -c rabbitmq/rabbitmq.sh -a create rabbitmq
+
