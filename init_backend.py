3cffa50517065f07d42e3fd9855f8da4e9a665fb
Samuel Scheidegger <scheidegger.samuel@gmail.com>
2018-01-24 13:54:00 +0100

    asdf

--- init_backend.py
+++ init_backend.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
 
     # parser.add_option('-c', '--initfile', dest='initFile',
     #                   help='Path to INITFILE', metavar='INITFILE', default="vm-init.sh")
-    parser.add_option('-r', '--rabbitname', dest='rabbitmq',
+    parser.add_option('-r', '--rabbitname', dest='rabbitname',
                       help='rabbitmq id',
                       default="list", metavar='RABBITMQ')
     (options, args) = parser.parse_args()
