ccf2f273bdbdeb5fa2a6587e4fe7eb4d713aeba4
Tommy Tram <tommy.tram@zenuity.com>
2018-01-25 02:37:53 +0100

    added logging to csv file to monitor

--- monitor.py
+++ monitor.py
@@ -4,7 +4,8 @@ import ConfigParser
 from vmanager import Manager
 from urllib2 import Request, urlopen, URLError
 import urlparse
-
+import os.path
+import time
 
 def start_vm(name, image, start_script):
 
@@ -139,13 +140,23 @@ if __name__ == "__main__":
 
     (options, args) = parser.parse_args()
 
-    free, busy, na, cpu = get_stats(
-        options.backendname, options.network, options.port)
+    log = open(time.strftime("%Y%m%d-%H%M%S") + ".csv","w+")
+    log.write("free,busy,N/A,queue,cpu \n")
+    try:
+        while (True):
 
-    queue = get_queue_length(options.credentialFile)
+            free, busy, na, cpu = get_stats(
+                options.backendname, options.network, options.port)
 
-    print("Free: {0} Busy: {1} N/A: {2} Queue: {3}".format(free, busy, na, queue))
+            queue = get_queue_length(options.credentialFile)
 
-    print(cpu)
+            print("Free: {0} Busy: {1} N/A: {2} Queue: {3}".format(free, busy, na, queue))
+
+            print(cpu)
+
+            log.write("{0},{1},{2},{3},{4}\n".format(free, busy, na, queue, cpu))
+            time.sleep(1)
+    except KeyboardInterrupt:
+        log.close()
 
     pass
